a0da6dd3da36ba0c5619325c825ea7f98c582622
alikhan-yessaly <alikhan.yessaly@quantum.edu.kz>
2024-10-08 21:06:23 +0500

    Update manifest_picow.py

--- micropython/board/manifest_picow.py
+++ micropython/board/manifest_picow.py
@@ -3,5 +3,10 @@ freeze("../modules_py", "interstate75.py")
 freeze("../modules_py", "automation.py")
 freeze("../modules_py", "inventor.py")
 
+freeze("../modules_py", "imu.py")
+freeze("../modules_py", "rtbot.py")
+freeze("../modules_py", "display.py")
+freeze("../modules_py", "distance.py")
+
 freeze("../modules_py", "pimoroni.py")
 freeze("../modules_py", "boot.py")
