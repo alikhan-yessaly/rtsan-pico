ce668efc5ad4e678fc8c2b14253c7b5bba0a319e
alikhan-yessaly <alikhan.yessaly@quantum.edu.kz>
2024-10-08 22:52:54 +0500

    Removed empty spaces

--- micropython/board/manifest_picow.py
+++ micropython/board/manifest_picow.py
@@ -6,11 +6,9 @@ freeze("../modules_py", "imu.py")
 freeze("../modules_py", "rtbot.py")
 freeze("../modules_py", "display.py")
 freeze("../modules_py", "distance.py")
-
 freeze("../modules_py", "imu.py")
 freeze("../modules_py", "rtbot.py")
 freeze("../modules_py", "display.py")
 freeze("../modules_py", "distance.py")
-
 freeze("../modules_py", "pimoroni.py")
 freeze("../modules_py", "boot.py")
