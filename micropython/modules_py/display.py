a9b968419673f35264abada5a7253c7f14f5823a
alikhan-yessaly <alikhan.yessaly@quantum.edu.kz>
2024-10-08 23:22:32 +0500

    Fixed issues in display module

--- micropython/modules_py/display.py
+++ micropython/modules_py/display.py
@@ -33,11 +33,14 @@ class Display(framebuf.FrameBuffer):
     except:
         print("Oled display is not connected! Check the connection!")
 
-    def __init__(self, width=128, height=32, external_vcc=i2c):
+    def __init__(self, width=128, height=32, external_vcc=False):
         self.width = width
         self.height = height
+        self.addr=0x3C
         self.external_vcc = external_vcc
         self.pages = self.height // 8
+        self.write_list = [b"\x40", None]  # Co=0, D/C#=1
+        self.temp = bytearray(2)
         self.buffer = bytearray(self.pages * self.width)
         super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
         self.init_display()
@@ -78,6 +81,15 @@ class Display(framebuf.FrameBuffer):
             self.write_cmd(cmd)
         self.fill(0)
         self.show()
+        
+    def write_cmd(self, cmd):
+        self.temp[0] = 0x80  # Co=1, D/C#=0
+        self.temp[1] = cmd
+        self.i2c.writeto(self.addr, self.temp)
+
+    def write_data(self, buf):
+        self.write_list[1] = buf
+        self.i2c.writevto(self.addr, self.write_list)
 
     def poweroff(self):
         self.write_cmd(SET_DISP | 0x00)
@@ -110,21 +122,3 @@ class Display(framebuf.FrameBuffer):
         self.write_cmd(0)
         self.write_cmd(self.pages - 1)
         self.write_data(self.buffer)
-
-
-class SSD1306_I2C(Display):
-    def __init__(self, width, height, i2c, addr=0x3C, external_vcc=False):
-        self.i2c = i2c
-        self.addr = addr
-        self.temp = bytearray(2)
-        self.write_list = [b"\x40", None]  # Co=0, D/C#=1
-        super().__init__(width, height, external_vcc)
-
-    def write_cmd(self, cmd):
-        self.temp[0] = 0x80  # Co=1, D/C#=0
-        self.temp[1] = cmd
-        self.i2c.writeto(self.addr, self.temp)
-
-    def write_data(self, buf):
-        self.write_list[1] = buf
-        self.i2c.writevto(self.addr, self.write_list)
